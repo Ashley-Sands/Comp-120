e2ae05580ea80162604da47b1e55322ccccbeeef
Ashley Sands <ashsands1@amstrike.com>
2018-11-17 04:15:34 +0000

    Change tinkered

--- tinkering_2.py
+++ tinkering_2.py
@@ -8,7 +8,9 @@ import wave_ext
 #((8 * get_sin_wave_tone(current_sample, sample_rate, frequency)) / math.pi) * math.asin(math.sin((8*math.pi*sample_rate)/current_sample))
 
 pygame.init()
-screen = pygame.display.set_mode((1000, 150))
+screen = pygame.display.set_mode((1000, 250))
+fps_clock = pygame.time.Clock()
+FPS = 60
 
 BASE_FREQUENCIES = {}
 BASE_FREQUENCIES["C"] = 16.35
@@ -88,11 +90,13 @@ def get_triangle_wave_tone(current_sample, sample_rate, frequency):
     current_sample += 1
     return (2.0*MAX_DEPTH/math.pi) * math.asin((math.sin(2.0*math.pi*current_sample/(sample_rate/frequency)))) * (MAX_DEPTH)
 
+
 def get_saw_wave_tone(current_sample, sample_rate, frequency):
 
     current_sample += 1
     tan = math.tan(current_sample*math.pi/(sample_rate/frequency))
     return -(2.0*MAX_DEPTH/math.pi) * math.atan(12.0/tan) * (MAX_DEPTH)
+
 
 def triangle_tone(freq_1_name, freq_1_key, sample_rate, length):
 
@@ -102,9 +106,9 @@ def triangle_tone(freq_1_name, freq_1_key, sample_rate, length):
         sound.add_sample(get_triangle_wave_tone(i, sample_rate, get_tone_by_key(freq_1_key, freq_1_name)))
 
     sound.normalize((MAX_DEPTH * 0.9))
-    sound.write_sample_data("audio/two_tones__", 1, sample_rate)
+    # sound.write_sample_data("audio/two_tones__", channels=1, sample_rate=sample_rate)
 
-    return sound.sample_data[0:1000]
+    return sound
 
 
 def saw_tone(freq_1_name, freq_1_key, sample_rate, length):
@@ -115,9 +119,9 @@ def saw_tone(freq_1_name, freq_1_key, sample_rate, length):
         sound.add_sample(get_saw_wave_tone(i, sample_rate, get_tone_by_key(freq_1_key, freq_1_name)))
 
     sound.normalize((MAX_DEPTH * 0.9))
-    sound.write_sample_data("audio/two_tones__", 1, sample_rate)
+    # sound.write_sample_data("audio/two_tones__", channels=1, sample_rate=sample_rate)
 
-    return sound.sample_data[0:1000]
+    return sound
 
 
 def dual_tone(freq_1_name, freq_1_key, freq_2_name, freq_2_key, sample_rate, length):
@@ -129,9 +133,9 @@ def dual_tone(freq_1_name, freq_1_key, freq_2_name, freq_2_key, sample_rate, len
         sound.combine_samples(i, get_sin_wave_tone(i, sample_rate, get_tone_by_key(freq_2_key, freq_2_name)))
 
     sound.normalize((MAX_DEPTH * 0.9))
-    sound.write_sample_data("audio/two_tones__", 1, sample_rate)
+    #sound.write_sample_data("audio/two_tones__", channels=1, sample_rate=sample_rate)
 
-    return sound.sample_data[0:1500]
+    return sound
 
 
 def echo():
@@ -160,6 +164,21 @@ def get_tone_by_key(key, tone_key):
 
     return base_tone
 
+def generate_tone(tone, freq_name, freq_key, sample_rate=44100, length=1):
+
+    if tone == "dual":
+        return dual_tone(freq_name, freq_key, "E", freq_key + 1, sample_rate, length)
+    elif tone == "saw":
+        return saw_tone(freq_name, freq_key, sample_rate, length)
+    elif tone == "triangle":
+        return triangle_tone(freq_name, freq_key, sample_rate, length)
+
+        print("tone tone found")
+
+    empty_wav = wave_ext.ReadWriteWav()
+    empty_wav.sample_data = [0]
+
+    return empty_wav
 
 def draw_wave_to_screen(width, height, audio_wave, max_vol):
 
@@ -181,13 +200,13 @@ def draw_wave_to_screen(width, height, audio_wave, max_vol):
 
         y = int( (1 - precent) * height)
 
-        pixel_color = (255 * precent, 255, 255 * (1-precent))
+        pixel_color = (255 * precent, 0, 255 * (1-precent))
 
         pixel_array[x, height // 2] = (100, 100, 100)
-        pixel_array[x, y] = pixel_color
+        pygame.draw.line(surface, pixel_color, (x, height // 2), (x, y))
+        # pixel_array[x, y] = pixel_color
 
         x += 1
-        print("aa", x, y)
         if x > width-1:
             break
 
@@ -196,6 +215,47 @@ def draw_wave_to_screen(width, height, audio_wave, max_vol):
     screen.blit(surface, (0, 0))
     pygame.display.flip()
 
+
+preview_start = 0
+preview_display = 1000
+preview_end = preview_start + preview_display
+preview_max = 1
+previewSamples = wave_ext.ReadWriteWav()
+previewSamples.sample_data = [0]
+
+prv_time = 0
+prv_pass = 0;
+delta_time = 0
+
+tone_type = "dual"
+frequency_name = "C"
+frequency_key = 5
+
+audio_stream = b''
+sound = pygame.mixer.Sound(audio_stream)
+sound.play(loops=100)
+
+update_audio_stream = False
+key_down = False
+
+fontface = pygame.font.SysFont("arial", 35)
+
+key_sounds = {}
+key_audio_stream = {}
+
+
+def setup_key_streams():
+
+    keys = list(BASE_FREQUENCIES)
+
+    for key in keys:
+        key_audio_stream[key] = b''
+        key_sounds[key] = {"audio": pygame.mixer.Sound(key_audio_stream[key]), "isPlaying": False, "freqKey":-999}
+
+def play_key_press(freq_name, freq_key, tone_type):
+    pass
+    #if key_sounds[frequency_name]["freqKey"] != freq_key:
+    #    key_sounds =
 
 while True:
 
@@ -206,11 +266,103 @@ while True:
             sys.exit()
         # change the key pressed state
         elif event.type == KEYDOWN:
+
+            if event.key == K_1:
+                tone_type = "dual"
+            elif event.key == K_2:
+                tone_type = "saw"
+            elif event.key == K_3:
+                tone_type = "triangle"
+
+            if event.key == K_EQUALS:
+                frequency_key += 1
+                if key_down:
+                    update_audio_stream = True
+            elif event.key == K_MINUS:
+                frequency_key -= 1
+                if key_down:
+                    update_audio_stream = True
+
+            if event.key == K_f:
+                frequency_name = "C"
+            if event.key == K_r:
+                frequency_name = "C#"
+
+            if event.key == K_g:
+                frequency_name = "D"
+            if event.key == K_t:
+                frequency_name = "D#"
+
+            if event.key == K_h:
+                frequency_name = "E"
+
+            if event.key == K_j:
+                frequency_name = "F"
+            if event.key == K_u:
+                frequency_name = "F#"
+
+            if event.key == K_k:
+                frequency_name = "G"
             if event.key == K_i:
-                #sound_to_mono()
-                #echo()
-                #previewSamples = dual_tone("C", 2, "E", 4, 44100, 2.5)
-                #previewSamples = triangle_tone("C", 0, 44100, 2.5)
-                previewSamples = saw_tone("C", 4, 44100, 2.5)
-                draw_wave_to_screen(1000, 150, previewSamples, MAX_DEPTH)
+                frequency_name = "G#"
+
+            if event.key == K_l:
+                frequency_name = "A"
+            if event.key == K_o:
+                frequency_name = "A#"
+
+            if event.key == K_SEMICOLON:
+                frequency_name = "B"
+
+
+            #sound_to_mono()
+            #echo()
+
+            #previewSamples = triangle_tone("C", 0, 44100, 2.5)
+            #previewSamples = saw_tone("C", 4, 44100, 2.5)
+            #previewSamples = dual_tone("C", 2, "E", 4, 44100, 2.5)
+
+            if event.key == K_f or event.key == K_r or event.key == K_g or event.key == K_t or event.key == K_h or event.key == K_u or event.key == K_j or event.key == K_i \
+                    or event.key == K_k or event.key == K_l  or event.key == K_o or event.key == K_SEMICOLON or update_audio_stream:
+                previewSamples = generate_tone(tone_type, frequency_name, frequency_key)
+                preview_start = 0
+                preview_max = len(previewSamples.sample_data)
+                previewSamples.encode_samples()
+                audio_stream = b''.join(previewSamples.encoded_data)
+                sound.stop()
+                sound = pygame.mixer.Sound(audio_stream)
+                sound.play(loops=-1)
+                update_audio_stream = False
+                key_down = True
                 print("Done!")
+
+        elif event.type == KEYUP:
+            if event.key == K_f or event.key == K_r or event.key == K_g or event.key == K_t or event.key == K_h or event.key == K_u or event.key == K_j or event.key == K_i \
+                    or event.key == K_k or event.key == K_l  or event.key == K_o or event.key == K_SEMICOLON:
+                sound.stop()
+                previewSamples = wave_ext.ReadWriteWav()
+                previewSamples.sample_data = [0]
+                key_down = False
+
+    prv_pass += 441 * delta_time
+
+    if prv_pass > 1:
+        preview_start += math.floor(prv_pass)
+        prv_pass -= math.floor(prv_pass)
+
+    preview_end = preview_start + preview_display
+    if preview_start > preview_max:
+        preview_start -= preview_max
+
+    draw_wave_to_screen(1000, 150, previewSamples.sample_data[preview_start:preview_end], MAX_DEPTH)
+
+    text = "Tone: "+tone_type+" Key: "+frequency_name+str(frequency_key)
+    text_surface = pygame.Surface((1000, 150))
+    text_surface.fill((25, 25, 25))
+    text_surface = fontface.render(text, True, (255, 255, 255))
+
+    pygame.draw.rect(screen, (25, 25, 25), (0, 150, 1000, 100), 0)
+    screen.blit(text_surface, (0, 150))
+
+    fps_clock.tick(FPS)
+    delta_time = fps_clock.get_time() / 1000
