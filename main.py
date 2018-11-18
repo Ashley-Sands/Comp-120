82a43e0db724c2df01536cf435ea1aa2e0483e56
Ashley Sands <ashsands1@amstrike.com>
2018-11-18 20:51:17 +0000

    initial commit for Main

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.locals import *
+import wave_ext
+import soundFX
+import wave
+
+WINDOW_HEIGHT = 750
+WINDOW_WIDTH = 1334
+
+pygame.init()
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+fps_clock = pygame.time.Clock()
+FPS = 60
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
