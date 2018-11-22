5e8f969f6a541276cc1a68f4f7f2d917c9d17b10
Ashley Sands <ashsands1@amstrike.com>
2018-11-22 00:04:39 +0000

    Add little tune

--- main.py
+++ main.py
@@ -53,11 +53,47 @@ synth["sine"] = { "Key": 3, "harmonic_steps": 1}
 synth["saw"] = { "Key": 3, "harmonic_steps": 1}
 synth["triangle"] = { "Key": 3, "harmonic_steps": 1}
 
-adsr_envelope = soundFX.ADSR_envelope(SAMPLE_RATE, 1, 0, 1, 1, 0.75, 1, 0.5, 1, 0)
+adsr_envelope = soundFX.ADSR_envelope(SAMPLE_RATE, 0.1, 0, 1, .1, 0.75, 0.2, 0.25, .2, 0)
+adsr_saw_envelope = soundFX.ADSR_envelope(SAMPLE_RATE, 0.3, 0, 1, .1, .7, 0.2, 0.1, 0.1, 0)
+adsr_sine_envelope = soundFX.ADSR_envelope(SAMPLE_RATE, 0.3, 0, 1, .1, 1, 0.2, 0.25, 0.2, 0)
+adsr_bass_envelope = soundFX.ADSR_envelope(SAMPLE_RATE, 0.1, 0, 1, 0.25, 0.75, 1, 0.5, 0.1, 0)
 
-timeline = [{"start_sample": 0, "length": SAMPLE_RATE * 4, "base_freq": "F", "key": 1, "harmonic_steps": 5, "wave_shape": "triangle", "envelope": adsr_envelope}]
-#timeline.append({"start_sample": SAMPLE_RATE*3, "length": SAMPLE_RATE * 4, "base_freq": "C", "key": 3, "harmonic_steps": 1, "wave_shape": "sine", "envelope": None})
-#timeline.append({"start_sample": SAMPLE_RATE*6, "length": SAMPLE_RATE * 4, "base_freq": "F", "key": 1, "harmonic_steps": 5, "wave_shape": "sine", "envelope": None})
+timeline = []
+timeline.append({"start_sample": 0, "length": SAMPLE_RATE * 0.025, "base_freq": "D", "key": 3, "harmonic_steps": 2, "wave_shape": "triangle", "envelope": adsr_envelope, "velocity": .5})
+timeline.append({"start_sample": SAMPLE_RATE * 1.25, "length": SAMPLE_RATE * 0.015, "base_freq": "D", "key": 3, "harmonic_steps": 2, "wave_shape": "triangle", "envelope": adsr_envelope, "velocity": 0.5})
+timeline.append({"start_sample": SAMPLE_RATE * 2.5, "length": SAMPLE_RATE * 0.015, "base_freq": "D", "key": 3, "harmonic_steps": 2, "wave_shape": "triangle", "envelope": adsr_envelope, "velocity": 0.5})
+timeline.append({"start_sample": SAMPLE_RATE * 2.95, "length": SAMPLE_RATE * 0.015, "base_freq": "D", "key": 2, "harmonic_steps": 2, "wave_shape": "triangle", "envelope": adsr_envelope, "velocity": 0.6})
+timeline.append({"start_sample": SAMPLE_RATE * 3.75, "length": SAMPLE_RATE * 0.015, "base_freq": "D", "key": 3, "harmonic_steps": 2, "wave_shape": "triangle", "envelope": adsr_envelope, "velocity": 0.5})
+
+timeline.append({"start_sample": SAMPLE_RATE * 1.25, "length": SAMPLE_RATE * 0.015, "base_freq": "D", "key": 2, "harmonic_steps": 1, "wave_shape": "triangle", "envelope": adsr_envelope, "velocity": .8})
+timeline.append({"start_sample": SAMPLE_RATE * 1.5, "length": SAMPLE_RATE * 0.015, "base_freq": "D", "key": 2, "harmonic_steps": 1, "wave_shape": "triangle", "envelope": adsr_envelope, "velocity": .8})
+timeline.append({"start_sample": SAMPLE_RATE * 1.75, "length": SAMPLE_RATE * 0.015, "base_freq": "D", "key": 2, "harmonic_steps": 1, "wave_shape": "triangle", "envelope": adsr_envelope, "velocity": .8})
+
+timeline.append({"start_sample": 0, "length": SAMPLE_RATE * 1, "base_freq": "E", "key": 1, "harmonic_steps": 2, "wave_shape": "sine", "envelope": adsr_bass_envelope, "velocity": 1})
+timeline.append({"start_sample": SAMPLE_RATE * 1.25, "length": SAMPLE_RATE * 1, "base_freq": "G", "key": 1, "harmonic_steps": 2, "wave_shape": "sine", "envelope": adsr_bass_envelope, "velocity": 1})
+timeline.append({"start_sample": SAMPLE_RATE * 2.5, "length": SAMPLE_RATE * 1, "base_freq": "F", "key": 1, "harmonic_steps": 2, "wave_shape": "sine", "envelope": adsr_bass_envelope, "velocity": 1})
+timeline.append({"start_sample": SAMPLE_RATE * 3.75, "length": SAMPLE_RATE * 1, "base_freq": "D", "key": 1, "harmonic_steps": 2, "wave_shape": "sine", "envelope": adsr_bass_envelope, "velocity": 1})
+
+timeline.append({"start_sample": SAMPLE_RATE * 4.5, "length": SAMPLE_RATE * 1, "base_freq": "F", "key": 1, "harmonic_steps": 2, "wave_shape": "sine", "envelope": adsr_bass_envelope, "velocity": 1})
+
+timeline.append({"start_sample": SAMPLE_RATE * 0.75, "length": SAMPLE_RATE * 0.1, "base_freq": "D", "key": 2, "harmonic_steps": 1, "wave_shape": "sine", "envelope": adsr_saw_envelope, "velocity": .7})
+timeline.append({"start_sample": SAMPLE_RATE * 2, "length": SAMPLE_RATE * 0.1, "base_freq": "D", "key": 2, "harmonic_steps": 1, "wave_shape": "sine", "envelope": adsr_saw_envelope, "velocity": .7})
+timeline.append({"start_sample": SAMPLE_RATE * 3.25, "length": SAMPLE_RATE * 0.1, "base_freq": "F", "key": 2, "harmonic_steps": 1, "wave_shape": "sine", "envelope": adsr_saw_envelope, "velocity": .7})
+
+timeline.append({"start_sample": SAMPLE_RATE * 4, "length": SAMPLE_RATE * 0.75, "base_freq": "D", "key": 1, "harmonic_steps": 1, "wave_shape": "saw", "envelope": adsr_saw_envelope, "velocity": .3})
+
+timeline.append({"start_sample": SAMPLE_RATE * 0, "length": SAMPLE_RATE * 0.1, "base_freq": "D", "key": 1, "harmonic_steps": 1, "wave_shape": "saw", "envelope": adsr_saw_envelope, "velocity": .6})
+timeline.append({"start_sample": SAMPLE_RATE * 1.25, "length": SAMPLE_RATE * 0.0125, "base_freq": "D", "key": 3, "harmonic_steps": 1, "wave_shape": "saw", "envelope": adsr_saw_envelope, "velocity": .5})
+timeline.append({"start_sample": SAMPLE_RATE * 2.5, "length": SAMPLE_RATE * 0.0125, "base_freq": "D", "key": 3, "harmonic_steps": 1, "wave_shape": "saw", "envelope": adsr_saw_envelope, "velocity": .5})
+timeline.append({"start_sample": SAMPLE_RATE * 3.75, "length": SAMPLE_RATE * 0.0125, "base_freq": "D", "key": 3, "harmonic_steps": 1, "wave_shape": "saw", "envelope": adsr_saw_envelope, "velocity": .5})
+timeline.append({"start_sample": SAMPLE_RATE * 4.85, "length": SAMPLE_RATE * 0.1, "base_freq": "D", "key": 2, "harmonic_steps": 2, "wave_shape": "saw", "envelope": adsr_saw_envelope, "velocity": .4})
+
+timeline.append({"start_sample": SAMPLE_RATE * 1, "length": SAMPLE_RATE * 0.1, "base_freq": "F", "key": 4, "harmonic_steps": 1, "wave_shape": "sine", "envelope": adsr_sine_envelope, "velocity": .7})
+timeline.append({"start_sample": SAMPLE_RATE * 2.25, "length": SAMPLE_RATE * 0.1, "base_freq": "F", "key": 4, "harmonic_steps": 1, "wave_shape": "sine", "envelope": adsr_sine_envelope, "velocity": .7})
+timeline.append({"start_sample": SAMPLE_RATE * 2.6, "length": SAMPLE_RATE * 0.1, "base_freq": "F", "key": 3, "harmonic_steps": 1, "wave_shape": "sine", "envelope": adsr_sine_envelope, "velocity": .7})
+timeline.append({"start_sample": SAMPLE_RATE * 3.25, "length": SAMPLE_RATE * 0.1, "base_freq": "F", "key": 3, "harmonic_steps": 1, "wave_shape": "sine", "envelope": adsr_sine_envelope, "velocity": .7})
+timeline.append({"start_sample": SAMPLE_RATE * 3.45, "length": SAMPLE_RATE * 0.025, "base_freq": "F", "key": 3, "harmonic_steps": 1, "wave_shape": "sine", "envelope": adsr_saw_envelope, "velocity": .7})
+timeline.append({"start_sample": SAMPLE_RATE * 3.65, "length": SAMPLE_RATE * 0.025, "base_freq": "F", "key": 3, "harmonic_steps": 1, "wave_shape": "sine", "envelope": adsr_saw_envelope, "velocity": .7})
 
 # build audio
 wave_lib = waves.WaveLibrary(SAMPLE_RATE, MAX_DEPTH)
@@ -104,7 +140,7 @@ def render():
         combine_audio(tone, timeline[i]["start_sample"])
 
     audio.normalize(MAX_DEPTH * 0.9)
-    audio.write_sample_data("audio/main", sample_rate=SAMPLE_RATE)
+    audio.write_sample_data("audio/main_1", sample_rate=SAMPLE_RATE)
 
     print("Render Complete!")
 
