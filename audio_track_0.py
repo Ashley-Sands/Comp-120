1749490cd3e848a9d064f23bcade8fb1a61b4fff
Ashley Sands <ashsands1@amstrike.com>
2018-11-23 00:31:06 +0000

    Added 2 Sound Fx's

--- audio_track_0.py
+++ audio_track_0.py
@@ -2,12 +2,12 @@ import soundFX
 import wave_ext
 
 """
-Audio Track 1
+Audio Track 0
 Author:         Ashley Sands
-Track Name:     Critical Creepy 1
+Track Name:     Critical Creepy 0
 """
 
-TRACK_NAME = "critical_creepy_1.wav"
+TRACK_NAME = "critical_creepy_0.wav"
 
 COMBINE_ADD = wave_ext.ReadWriteWav.COMBINE_ADD
 COMBINE_SUB = wave_ext.ReadWriteWav.COMBINE_SUB
@@ -22,4 +22,3 @@ adsr_bass_envelope = soundFX.ADSR_envelope(SAMPLE_RATE, 0.1, 0, 1, 0.25, 0.75, 1
 timeline = []
 timeline.append({"start_sample": 0, "length": SAMPLE_RATE * 1, "base_freq": "D", "key": 2, "harmonic_steps": 1, "wave_shape": "triangle", "envelope": adsr_envelope, "velocity": .7, "combine": COMBINE_ADD, "echo": (0, SAMPLE_RATE * .5, SAMPLE_RATE * 0.15, 0.99999)})
 timeline.append({"start_sample": 0, "length": SAMPLE_RATE * 1, "base_freq": "D", "key": 1, "harmonic_steps": 2, "wave_shape": "sine", "envelope": adsr_envelope, "velocity": .7, "combine": COMBINE_SUB, "echo": (0, SAMPLE_RATE * .5, SAMPLE_RATE * 0.25, 0.99999)})
-timeline.append({"start_sample": 0, "length": SAMPLE_RATE * 1, "base_freq": "F", "key": 2, "harmonic_steps": 3, "wave_shape": "sine", "envelope": adsr_envelope, "velocity": .5, "combine": COMBINE_SUB, "echo": (0, SAMPLE_RATE * .15, SAMPLE_RATE * 0.25, 0.9999)})
