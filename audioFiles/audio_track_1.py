3a27c3877dfe2be2814665ba0b49e9df5b001973
Ashley Sands <ashsands1@amstrike.com>
2018-11-23 15:51:10 +0000

    Updated audio tracks to PEP8 standards

--- audioFiles/audio_track_1.py
+++ audioFiles/audio_track_1.py
@@ -14,12 +14,60 @@ COMBINE_SUB = wave_ext.ReadWriteWav.COMBINE_SUB
 
 SAMPLE_RATE = 44100
 
-adsr_envelope = soundFX.ADSR_Envelope(SAMPLE_RATE, 0.1, 0, 1, .1, 0.75, 0.2, 0.25, .2, 0)
-adsr_saw_envelope = soundFX.ADSR_Envelope(SAMPLE_RATE, 0.3, 0, 1, .1, .7, 0.2, 0.1, 0.1, 0)
-adsr_sine_envelope = soundFX.ADSR_Envelope(SAMPLE_RATE, 0.3, 0, 1, .1, 1, 0.2, 0.25, 0.2, 0)
-adsr_bass_envelope = soundFX.ADSR_Envelope(SAMPLE_RATE, 0.1, 0, 1, 0.25, 0.75, 1, 0.5, 0.1, 0)
+# Envelope pramas
+# Sample rate,
+# attack length, attack start value, attack end value
+# decay length, decay end value,
+# sustain length, sustain end value
+# release length, release end value
+adsr_envelope = soundFX.ADSR_Envelope(SAMPLE_RATE,
+                                      0.1, 0, 1,
+                                      .1, 0.75,
+                                      0.2, 0.25,
+                                      .2, 0
+                                      )
+
 
 timeline = []
-timeline.append({"start_sample": 0, "length": SAMPLE_RATE * 1, "base_freq": "D", "key": 2, "harmonic_steps": 1, "wave_shape": "triangle", "envelope": adsr_envelope, "velocity": .7, "combine": COMBINE_ADD, "echo": (0, SAMPLE_RATE * .5, SAMPLE_RATE * 0.15, 0.99999)})
-timeline.append({"start_sample": 0, "length": SAMPLE_RATE * 1, "base_freq": "D", "key": 1, "harmonic_steps": 2, "wave_shape": "sine", "envelope": adsr_envelope, "velocity": .7, "combine": COMBINE_SUB, "echo": (0, SAMPLE_RATE * .5, SAMPLE_RATE * 0.25, 0.99999)})
-timeline.append({"start_sample": 0, "length": SAMPLE_RATE * 1, "base_freq": "F", "key": 2, "harmonic_steps": 3, "wave_shape": "sine", "envelope": adsr_envelope, "velocity": .5, "combine": COMBINE_SUB, "echo": (0, SAMPLE_RATE * .15, SAMPLE_RATE * 0.25, 0.9999)})
+timeline.append(
+    {
+        "start_sample": 0,
+        "length": SAMPLE_RATE * 1,
+        "base_freq": "D",
+        "key": 2,
+        "harmonic_steps": 1,
+        "wave_shape": "triangle",
+        "envelope": adsr_envelope,
+        "velocity": 0.7,
+        "combine": COMBINE_ADD,
+        "echo": (0, SAMPLE_RATE * .5, SAMPLE_RATE * 0.15, 0.99999)
+    }
+)
+timeline.append(
+    {
+        "start_sample": 0,
+        "length": SAMPLE_RATE * 1,
+        "base_freq": "D",
+        "key": 1,
+        "harmonic_steps": 2,
+        "wave_shape": "sine",
+        "envelope": adsr_envelope,
+        "velocity": 0.7,
+        "combine": COMBINE_SUB,
+        "echo": (0, SAMPLE_RATE * .5, SAMPLE_RATE * 0.25, 0.99999)
+    }
+)
+timeline.append(
+    {
+        "start_sample": 0,
+         "length": SAMPLE_RATE * 1,
+         "base_freq": "F",
+         "key": 2,
+         "harmonic_steps": 3,
+         "wave_shape": "sine",
+         "envelope": adsr_envelope,
+         "velocity": 0.5,
+         "combine": COMBINE_SUB,
+         "echo": (0, SAMPLE_RATE * .15, SAMPLE_RATE * 0.25, 0.9999)
+     }
+)
--- audioFiles/audio_track_3.py
+++ audioFiles/audio_track_3.py
@@ -14,16 +14,95 @@ COMBINE_SUB = wave_ext.ReadWriteWav.COMBINE_SUB
 
 SAMPLE_RATE = 44100
 
-adsr_long_envelope = soundFX.ADSR_Envelope(SAMPLE_RATE, 0.2, 0, 1, .5, 0.75, 1, 0.5, .2, 0)
-adsr_envelope = soundFX.ADSR_Envelope(SAMPLE_RATE, 0.1, 0, 1, .1, 0.75, 0.2, 0.25, .2, 0)
-adsr_saw_envelope = soundFX.ADSR_Envelope(SAMPLE_RATE, 0.3, 0, 1, .1, .7, 0.2, 0.1, 0.1, 0)
-adsr_sine_envelope = soundFX.ADSR_Envelope(SAMPLE_RATE, 0.3, 0, 1, .1, 1, 0.2, 0.25, 0.2, 0)
-adsr_bass_envelope = soundFX.ADSR_Envelope(SAMPLE_RATE, 0.1, 0, 1, 0.25, 0.75, 1, 0.5, 0.1, 0)
+# Envelope pramas
+# Sample rate,
+# attack length, attack start value, attack end value
+# decay length, decay end value,
+# sustain length, sustain end value
+# release length, release end value
+adsr_envelope = soundFX.ADSR_Envelope(SAMPLE_RATE,
+                                      0.1, 0, 1,
+                                      0.1, 0.75,
+                                      0.2, 0.25,
+                                      0.2, 0
+                                      )
+adsr_long_envelope = soundFX.ADSR_Envelope(SAMPLE_RATE,
+                                           0.2, 0, 1,
+                                           .5, 0.75,
+                                           1, 0.5,
+                                           0.2, 0
+                                           )
 
 timeline = []
-timeline.append({"start_sample": 0, "length": SAMPLE_RATE * 1, "base_freq": "D", "key": 2, "harmonic_steps": 1, "wave_shape": "triangle", "envelope": adsr_envelope, "velocity": .7, "combine": COMBINE_ADD, "echo": (0, SAMPLE_RATE * .5, SAMPLE_RATE * 0.15, 0.99999)})
-timeline.append({"start_sample": 0, "length": SAMPLE_RATE * 1, "base_freq": "D", "key": 1, "harmonic_steps": 2, "wave_shape": "sine", "envelope": adsr_envelope, "velocity": .7, "combine": COMBINE_SUB, "echo": (0, SAMPLE_RATE * .5, SAMPLE_RATE * 0.25, 0.99999)})
-timeline.append({"start_sample": 0, "length": SAMPLE_RATE * 10, "base_freq": "D", "key": 2, "harmonic_steps": 3, "wave_shape": "saw", "envelope": adsr_long_envelope, "velocity": .2, "combine": COMBINE_ADD, "echo": (0, SAMPLE_RATE * .5, SAMPLE_RATE * 0.25, 0.99999)})
+timeline.append(
+    {
+        "start_sample": 0,
+        "length": SAMPLE_RATE * 1,
+        "base_freq": "D",
+        "key": 2,
+        "harmonic_steps": 1,
+        "wave_shape": "triangle",
+        "envelope": adsr_envelope,
+        "velocity": 0.7,
+        "combine": COMBINE_ADD,
+        "echo": (0, SAMPLE_RATE * 0.5, SAMPLE_RATE * 0.15, 0.99999)
+    }
+)
+timeline.append(
+    {
+        "start_sample": 0,
+        "length": SAMPLE_RATE * 1,
+        "base_freq": "D",
+        "key": 1,
+        "harmonic_steps": 2,
+        "wave_shape": "sine",
+        "envelope": adsr_envelope,
+        "velocity": 0.7,
+        "combine": COMBINE_SUB,
+        "echo": (0, SAMPLE_RATE * 0.5, SAMPLE_RATE * 0.25, 0.99999)
+    }
+)
+timeline.append(
+    {
+        "start_sample": 0,
+         "length": SAMPLE_RATE * 10,
+         "base_freq": "D",
+         "key": 2,
+         "harmonic_steps": 3,
+         "wave_shape": "saw",
+         "envelope": adsr_long_envelope,
+         "velocity": 0.2,
+         "combine": COMBINE_ADD,
+         "echo": (0, SAMPLE_RATE * 0.5, SAMPLE_RATE * 0.25, 0.99999)
+     }
+)
 
-timeline.append({"start_sample": 0, "length": SAMPLE_RATE * 6, "base_freq": "F", "key": 1, "harmonic_steps": 3, "wave_shape": "triangle", "envelope": adsr_long_envelope, "velocity": 1, "combine": COMBINE_ADD, "echo": (0, SAMPLE_RATE * .5, SAMPLE_RATE * 0.75, 0.9999)})
-timeline.append({"start_sample": 0, "length": SAMPLE_RATE * 1, "base_freq": "D", "key": 2, "harmonic_steps": 3, "wave_shape": "sine", "envelope": adsr_long_envelope, "velocity": .7, "combine": COMBINE_SUB, "echo": (0, SAMPLE_RATE * .25, SAMPLE_RATE * 0.75, 0.99999)})
+timeline.append(
+    {
+        "start_sample": 0,
+        "length": SAMPLE_RATE * 6,
+        "base_freq": "F",
+        "key": 1,
+        "harmonic_steps": 3,
+        "wave_shape": "triangle",
+        "envelope": adsr_long_envelope,
+        "velocity": 1,
+        "combine": COMBINE_ADD,
+        "echo": (0, SAMPLE_RATE * .5, SAMPLE_RATE * 0.75, 0.9999)
+    }
+)
+
+timeline.append(
+    {
+        "start_sample": 0,
+        "length": SAMPLE_RATE * 1,
+        "base_freq": "D",
+        "key": 2,
+        "harmonic_steps": 3,
+        "wave_shape": "sine",
+        "envelope": adsr_long_envelope,
+        "velocity": 0.7,
+        "combine": COMBINE_SUB,
+        "echo": (0, SAMPLE_RATE * 0.25, SAMPLE_RATE * 0.75, 0.99999)
+    }
+)
